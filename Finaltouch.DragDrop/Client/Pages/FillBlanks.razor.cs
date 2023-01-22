172637a665a42f57d313d458f176be6c79cf9780
Robert Lebowitz <lebowitz@finaltouch.com>
2023-01-22 11:30:29 -0600

    Adding drag word exercise

---
+++ Finaltouch.DragDrop/Client/Pages/FillBlanks.razor.cs
@@ -0,0 +1,6 @@
+﻿namespace Finaltouch.DragDrop.Client.Pages
+{
+    public partial class FillBlank
+    {
+    }
+}
---
+++ Finaltouch.DragDrop/Client/Shared/Word.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Finaltouch.DragDrop.Client.Shared
+{
+    //https://github.com/ChinmayMhatre/Unolingo/blob/master/components/Word.jsx
+    public partial class Word
+    {
+        [Parameter]
+        public string? Text { get; set; }
+        [Parameter]
+        public string? Hint { get; set; }
+    }
+}
